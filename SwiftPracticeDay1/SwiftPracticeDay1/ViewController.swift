cf23a178ee3a8eb7c659e32b5c88e8743bf48804
devhwi <zziller03@gmail.com>
2017-02-14 02:31:22 +0900

    first

---
+++ SwiftPracticeDay1/SwiftPracticeDay1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPracticeDay1
+//
+//  Created by Jonghwi Lee on 2017. 2. 14..
+//  Copyright © 2017년 Jonghwi Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
