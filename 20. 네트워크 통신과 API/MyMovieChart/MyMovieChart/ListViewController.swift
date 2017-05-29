92c55a5cf7e4fe1d4b6519269908cf3ff1c162d7
hwiVeloper <zziller03@gmail.com>
2017-05-29 17:54:40 +0900

    20강 MyMovieChart 더보기 버튼 추가

--- 20. 네트워크 통신과 API/MyMovieChart/MyMovieChart/ListViewController.swift
+++ 20. 네트워크 통신과 API/MyMovieChart/MyMovieChart/ListViewController.swift
@@ -11,11 +11,17 @@ import UIKit
 
 class ListViewController : UITableViewController {
     
+    // 더보기를 위해 페이지 정보 추가
+    var page = 1
+    
     lazy var list : [MovieVO] = {
         var datalist = [MovieVO]()
         
         return datalist
     } ()
+    
+    // 더보기 버튼
+    @IBOutlet var moreBtn: UIButton!
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        NSLog("\(self.list.count)")
@@ -93,6 +99,59 @@ class ListViewController : UITableViewController {
             }
         } catch {
             
+        }
+    }
+    @IBAction func more(_ sender: Any) {
+        // 페이지 값 추가
+        self.page += 1
+        
+        // API 호출을 위한 URI 생성
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=10&genreId=&order=releasedateasc"
+        let apiURI : URL! = URL(string: url)
+        
+        // API 호출
+        let apidata = try! Data(contentsOf: apiURI)
+        
+        // JSON 객체를 파싱하여 NSDictionary 객체로 받는다.
+        do {
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            
+            // 데이터 구조에 따라 차례대로 캐스팅하여 읽어온다.
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            
+            // Iterator 처리를 하면서 API 데이터를 MovieVO 객체에 저장
+            for row in movie {
+                // 순회 상수를 NSDictionary 타입으로 캐스팅
+                let r = row as! NSDictionary
+                
+                // 테이블 뷰 리스트를 구성할 방식
+                let mvo = MovieVO()
+                
+                // movie 각 배열에 데이터를 mvo에 할당
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = (r["ratingAverage"] as! NSString).doubleValue
+                
+                // list 배열에 추가
+                self.list.append(mvo)
+                
+                // 데이터를 다시 읽어오도록 테이블 뷰 갱신
+                self.tableView.reloadData()
+                
+                // 전체 데이터에 대한 카운트
+                let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+                
+                // totalCount가 읽어온 데이터 크기와 같거나 큰 경우 -> 더보기 버튼 숨김
+                if(self.list.count >= totalCount) {
+                    self.moreBtn.isHidden = true
+                }
+            }
+        } catch {
+            NSLog("Parse Error!")
         }
     }
 }
