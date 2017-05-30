ad3e69dda8740ff3808a6ecb4d95fd6fd85fd061
hwiVeloper <zziller03@gmail.com>
2017-05-30 15:31:26 +0900

    22강 웹 뷰 예외 처리

--- 22. 웹 뷰 - 앱 내장형 브라우저/MyMovieChart/MyMovieChart/DetailViewController.swift
+++ 22. 웹 뷰 - 앱 내장형 브라우저/MyMovieChart/MyMovieChart/DetailViewController.swift
@@ -24,10 +24,34 @@ class DetailViewController : UIViewController {
         navbar.title = self.mvo?.title
         
         // 요청할 url 값을 가져옴 (객체 생성)
-        let url = URL(string: (self.mvo.detail)!)
-        let req = URLRequest(url: url!)
         
-        // 웹 뷰에 요청된 url을 불러온다.
-        self.wv.loadRequest(req)
+        if let url = self.mvo?.detail {
+            if let urlObj = URL(string: url) {
+                let req = URLRequest(url: urlObj)
+                // 웹 뷰에 요청된 url을 불러온다.
+                self.wv.loadRequest(req)
+            } else { // url 형식이 잘못되었을 경우
+                let alert = UIAlertController(title: "오류", message: "잘못된 URL입니다.", preferredStyle: .alert)
+                let cancel = UIAlertAction(title: "확인", style: .cancel) {
+                    (_) in
+                    // 이전페이지로 돌려보낸다.
+                    _ = self.navigationController?.popViewController(animated: true)
+                }
+                
+                alert.addAction(cancel)
+                self.present(alert, animated: false, completion: nil)
+            }
+        } else { // url이 전달되지 않았을 때
+            // 경고창으로 알림 전달 후 영화목록으로 돌아간다.
+            let alert = UIAlertController(title: "오류", message: "필수 파라미터가 누락되었습니다.", preferredStyle: .alert)
+            let cancel = UIAlertAction(title: "확인", style: .cancel) {
+                (_) in
+                // 이전페이지로 돌려보낸다.
+                _ = self.navigationController?.popViewController(animated: true)
+            }
+            
+            alert.addAction(cancel)
+            self.present(alert, animated: false, completion: nil)
+        }
     }
 }
