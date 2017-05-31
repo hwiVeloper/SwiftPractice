fb468afdc65c624b14936bde4cac181d206089e2
hwiVeloper <zziller03@gmail.com>
2017-05-31 16:40:42 +0900

    25강 오토 리사이징 & 오토 레이아웃

---
+++ 25. 오토 레이아웃/AutoResizing-Practice/AutoResizing-Practice/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  AutoResizing-Practice
+//
+//  Created by Jonghwi Lee on 2017. 5. 31..
+//  Copyright © 2017년 Jonghwi Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
