b90eed87dff122b2a4d45158f15958940e2cf4f6
hwiVeloper <zziller03@gmail.com>
2017-12-01 21:43:28 +0900

    vol1, vol2 폴더구분

---
+++ vol2/MyMemory/MyMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by Jonghwi Lee on 2017. 12. 1..
+//  Copyright © 2017년 Jonghwi Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
