28db71a25a5384e13a66bdddf4916d00ee5f1900
Jonghwi Lee <zziller03@gmail.com>
2018-07-17 23:57:20 +0900

    Chapter06-SQLite 기초 및 리팩토링

--- vol2/Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
+++ vol2/Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
@@ -11,38 +11,61 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        var db: OpaquePointer? = nil // SQLite 연결 정보를 담을 객체
-        var stmt: OpaquePointer? = nil // 컴파일된 SQL을 담을 객체
-        
-        // 앱 내 문서 디렉터리 경로에서 SQLite DB 파일을 찾는다.
-//        let fileMgr = FileManager()
-//        let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first!
-//        let dbPath = docPathURL.appendingPathComponent("db.sqlite").path
-        let dbPath = "/Users/Hwi/db.sqlite"
-        
-        let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)"
-        
-        if sqlite3_open(dbPath, &db) == SQLITE_OK { // db 연결 성공시
-            if sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK { // SQL 컴파일이 성공시
-                if sqlite3_step(stmt) == SQLITE_DONE {
-                    print("Create Table Success!")
-                }
-                sqlite3_finalize(stmt)
-            } else {
-                print("Prepare Statement Fail!")
-            }
-            
-            // db 연결 해제
-            sqlite3_close(db)
-        } else {
-            print("Database Connect Fail!")
-            return
-        }
+        let dbPath = self.getDBPath()
+        self.dbExecute(dbPath: dbPath)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func dbExecute(dbPath: String) {
+        var db: OpaquePointer? = nil // SQLite 연결 정보를 담을 객체
+        guard sqlite3_open(dbPath, &db) == SQLITE_OK else {
+            print("Database Connect Fail")
+            return
+        }
+        
+        // 데이터베이스 연결 종료
+        defer {
+            print("Close Database Connection")
+            sqlite3_close(db)
+        }
+        
+        var stmt: OpaquePointer? = nil // 컴파일된 SQL을 담을 객체
+        
+        let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)"
+        
+        guard sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK else {
+            print("Prepare Statement Fail")
+            return
+        }
+        
+        // stmt 변수 해제
+        defer {
+            print("Finalize Statement")
+            sqlite3_finalize(stmt)
+        }
+        
+        if sqlite3_step(stmt) == SQLITE_DONE {
+            print("Create Table Success!")
+        }
+    }
+    
+    func getDBPath() -> String {
+        // 앱 내 문서 디렉터리 경로에서 SQLite DB 파일을 찾는다.
+        let fileMgr = FileManager()
+        let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let dbPath = docPathURL.appendingPathComponent("db.sqlite").path
+        
+        // 추가되는 부분
+        // dbPath 경로에 파일이 없다면 앱 번들에 만들어 둔 db.sqlite를 가져와 복사한다.
+        if fileMgr.fileExists(atPath: dbPath) == false {
+            let dbSource = Bundle.main.path(forResource: "db", ofType: "sqlite")
+            try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath)
+        }
+        return dbPath
     }
 }
 
