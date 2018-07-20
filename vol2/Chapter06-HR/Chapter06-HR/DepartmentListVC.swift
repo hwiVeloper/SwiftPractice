a508d90c880e0cee93b1bb5aaf1d5889ddb2f4b6
Jonghwi Lee <zziller03@gmail.com>
2018-07-21 00:39:28 +0900

    Chapter06-hr Department List 부분

--- vol2/Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
+++ vol2/Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
@@ -10,5 +10,88 @@ import Foundation
 import UIKit
 
 class DepartmentListVC: UITableViewController {
+    var departList: [(departCd: Int, departTitle: String, departAddr: String)]!
+    let departDAO = DepartmentDAO()
     
+    // UI 초기화 함수
+    func initUI() {
+        // 1. 내비게이션 타이틀용 레이블 속성 설정
+        let navTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+        navTitle.numberOfLines = 2
+        navTitle.textAlignment = .center
+        navTitle.font = UIFont.systemFont(ofSize: 14)
+        navTitle.text = "부서 목록\n" + " 총 \(self.departList.count) 개"
+        
+        // 2. 내비게이션 바 UI 설정
+        self.navigationItem.titleView = navTitle
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        // 3. 셀을 스와이프했을 때 편집 모드가 되도록 설정
+        self.tableView.allowsSelectionDuringEditing = true
+    }
+    
+    override func viewDidLoad() {
+        self.departList = self.departDAO.find() // 기존 저장된 부서 정보를 가져온다.
+        self.initUI()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.departList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // indexPath 매개변수가 가리키는 행에 대한 데이터를 읽어온다.
+        let rowData = self.departList[indexPath.row]
+        
+        // 셀 객체를 생성하고 데이터를 배치한다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DEPART_CELL")
+        
+        cell?.textLabel?.text = rowData.departTitle
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        
+        cell?.detailTextLabel?.text = rowData.departAddr
+        cell?.detailTextLabel?.font = UIFont.systemFont(ofSize: 12)
+        
+        return cell!
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // 1. 삭제할 행의 departCd를 구한다.
+        let departCd = self.departList[indexPath.row].departCd
+        
+        // 2. DB에서, 데이터 소스에서, 그리고 테이블 뷰에서 차례대로 삭제.
+        if departDAO.remove(departCd: departCd) {
+            self.departList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    // 신규 부서를 추가하는 메소드
+    @IBAction func add(_ sender: Any) {
+        let alert = UIAlertController(title: "신규 부서 등록", message: "신규 부서를 등록해 주세요", preferredStyle: .alert)
+        
+        alert.addTextField() { (tf) in tf.placeholder = "부서명" }
+        alert.addTextField() { (tf) in tf.placeholder = "주소" }
+        
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        alert.addAction(UIAlertAction(title: "확인", style: .default) { (_) in
+            let title = alert.textFields?[0].text // 부서명
+            let addr  = alert.textFields?[1].text // 주소
+            
+            if self.departDAO.create(title: title!, addr: addr!) {
+                self.departList = self.departDAO.find()
+                self.tableView.reloadData()
+                
+                let navTitle = self.navigationItem.titleView as! UILabel
+                navTitle.text = "부서 목록\n" + "총 \(self.departList.count) 개"
+            }
+            
+        })
+        self.present(alert,animated: false)
+    }
 }
