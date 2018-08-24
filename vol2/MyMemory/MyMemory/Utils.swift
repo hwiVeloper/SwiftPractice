485a85a5767682578a85d85f59d29f6b830516e9
Jonghwi Lee <zziller03@gmail.com>
2018-08-24 23:56:42 +0900

    Chapter08-MyMemory 알림창(alert) 코드 간결화

--- vol2/MyMemory/MyMemory/Utils.swift
+++ vol2/MyMemory/MyMemory/Utils.swift
@@ -15,4 +15,16 @@ extension UIViewController {
     func instanceTutorialVC(name: String) ->UIViewController? {
         return self.tutorialSB.instantiateViewController(withIdentifier: name)
     }
+    
+    func alert(_ message: String, completion: (()->Void)? = nil) {
+        // 메인 스레드에서 실행되도록 한다.
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
+                completion?() // completion 매개변수의 값이 nil이 아닐 때에만 실행되도록
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: false)
+        }
+    }
 }
