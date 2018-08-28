81adea8046ec45d7b1c799f9d6ff101b5d27d762
Jonghwi Lee <zziller03@gmail.com>
2018-08-29 00:05:40 +0900

    Chapter08-KeyChain을 활용한 인증 토큰 처리

--- vol2/MyMemory/MyMemory/UserInfoManager.swift
+++ vol2/MyMemory/MyMemory/UserInfoManager.swift
@@ -110,6 +110,17 @@ class UserInfoManager {
                         self.profile = UIImage(data: imageData)
                     }
                 }
+                
+                // => 추가
+                // 토큰 정보 추출
+                let accessToken = jsonObject["access_token"] as! String
+                let refreshToken = jsonObject["refresh_token"] as! String
+                
+                // 토큰 정보 저장
+                let tk = TokenUtils()
+                tk.save("kr.co.rubypaper.MyMemory", account: "accessToken", value: accessToken)
+                tk.save("kr.co.rubypaper.MyMemory", account: "refreshToken", value: refreshToken)
+                
                 success?()
             } else {
                 let msg = (jsonObject["error_msg"] as? String) ?? "로그인이 싪패했습니다."
--- vol2/MyMemory/MyMemory/Utils.swift
+++ vol2/MyMemory/MyMemory/Utils.swift
@@ -11,7 +11,68 @@ import Security
 import Alamofire
 
 class TokenUtils {
+    func save(_ service: String, account: String, value: String) {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecValueData: value.data(using: .utf8, allowLossyConversion: false)!
+        ]
+        
+        // 현재 저장된 값 삭제
+        SecItemDelete(keyChainQuery)
+        
+        // 새로운 키체인 등록
+        let status: OSStatus = SecItemAdd(keyChainQuery, nil)
+        assert(status == noErr, "토큰 값 저장에 실패했습니다.")
+        NSLog("status = \(status)")
+    }
     
+    func load(_ service: String, account: String) -> String? {
+        // 키체인 쿼리 정의
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: kCFBooleanTrue,
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
+        
+        // 키체인에 저장된 값을 읽어옴
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(keyChainQuery, &dataTypeRef)
+        
+        // 처리 결과가 성공이라면 읽어온 값을 Data 타입으로 변환, 다시 String 타입으로 변환
+        if (status == errSecSuccess) {
+            let retrievedData = dataTypeRef as! Data
+            let value = String(data: retrievedData, encoding: String.Encoding.utf8)
+            return value
+        } else {
+            print("Nothing was retrieved from the keychain, Status code \(status)")
+            return nil
+        }
+    }
+    
+    func delete(_ service: String, account: String) {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account
+        ]
+        
+        // 현재 저장되어 있는 값 삭제
+        let status = SecItemDelete(keyChainQuery)
+        assert(status == noErr, "토큰 값 삭제에 실패했습니다.")
+        NSLog("status = \(status)")
+    }
+    
+    func getAuthorizationHeader() -> HTTPHeaders? {
+        let serviceID = "kr.co.rubypaper.MyMemory"
+        if let accessToken = self.load(serviceID, account: "accessToken") {
+            return ["Authorization" : "Bearer \(accessToken)"] as HTTPHeaders
+        } else {
+            return nil
+        }
+    }
 }
 
 extension UIViewController {
